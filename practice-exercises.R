af2a47076f329e528357cad5fefdb9dc1bff6420
JeffreyCHoover <43454055+JeffreyCHoover@users.noreply.github.com>
2023-10-17 10:41:49 -0400

    Adding base files

---
+++ practice-exercises.R
@@ -0,0 +1,17 @@
+# Practice Exercises
+
+## Use the `usethis` package to create a new branch
+
+## Create 3 folders within the main project folder
+### A folder titled "R"
+### A folder titled "data"
+### A folder titled "figures"
+
+## Create a .R script containing any sort of code/analyses that you'd like
+
+## Push the branch to Github
+
+## Create a Pull Request on Github to merge this branch into the main branch
+### Tag me for review of the branch
+
+## Once the branch is merged, use the `usethis` package to merge main
